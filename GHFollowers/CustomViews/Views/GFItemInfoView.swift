f669d672e8d380b93a63d5e64c8353d3c4a94af4
arvnq <arvin.quiliza@gmail.com>
2020-03-01 03:12:06 +1100

    Developed Item Info Views

---
+++ GHFollowers/CustomViews/Views/GFItemInfoView.swift
@@ -0,0 +1,75 @@
+//
+//  GFItemInfoView.swift
+//  GHFollowers
+//
+//  Created by Arvin Quiliza on 3/1/20.
+//  Copyright © 2020 arvinq. All rights reserved.
+//
+
+import UIKit
+
+
+class GFItemInfoView: UIView {
+    
+    var symbolImageView: UIImageView!
+    var titleLabel: GFTitleLabel!
+    var countLabel: GFTitleLabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureViews()
+        configureConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureViews() {
+        symbolImageView = UIImageView()
+        symbolImageView.translatesAutoresizingMaskIntoConstraints = false
+        symbolImageView.tintColor = .label
+        self.addSubview(symbolImageView)
+        
+        titleLabel = GFTitleLabel(textAlignment: .left, fontSize: 14)
+        self.addSubview(titleLabel)
+        
+        countLabel = GFTitleLabel(textAlignment: .center, fontSize: 14)
+        self.addSubview(countLabel)
+    }
+    
+    func configureConstraints() {
+        NSLayoutConstraint.activate([
+            symbolImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            symbolImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            symbolImageView.widthAnchor.constraint(equalToConstant: 20),
+            symbolImageView.heightAnchor.constraint(equalToConstant: 20),
+            
+            titleLabel.centerYAnchor.constraint(equalTo: symbolImageView.centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: symbolImageView.trailingAnchor, constant: 12),
+            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+//            titleLabel.heightAnchor.constraint(equalToConstant: 18),
+            
+            countLabel.topAnchor.constraint(equalTo: symbolImageView.bottomAnchor, constant: 4),
+            countLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            countLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+//            countLabel.heightAnchor.constraint(equalToConstant: 18)
+        ])
+    }
+    
+    func set(itemInfoType: ItemInfoType, withCount count: Int) {
+        switch itemInfoType {
+            case .repos:
+                symbolImageView.image = UIImage(systemName: SFSymbols.repos)
+            case .gists:
+                symbolImageView.image = UIImage(systemName: SFSymbols.gists)
+            case .follower:
+                symbolImageView.image = UIImage(systemName: SFSymbols.follower)
+            case .following:
+                symbolImageView.image = UIImage(systemName: SFSymbols.following)
+        }
+        
+        titleLabel.text = itemInfoType.rawValue
+        countLabel.text = String(count)
+    }
+}
--- GHFollowers/Utilities/Constants.swift
+++ GHFollowers/Utilities/Constants.swift
@@ -9,5 +9,16 @@
 import Foundation
 
 enum SFSymbols {
-    static let location = "mappin.and.ellipse"
+    static let location  = "mappin.and.ellipse"
+    static let repos     = "folder"
+    static let gists     = "text.alignleft"
+    static let follower  = "heart"
+    static let following = "person.2"
+}
+
+enum ItemInfoType: String {
+    case repos     = "Public Repose"
+    case gists     = "Public Gists"
+    case follower  = "Followers"
+    case following = "Following"
 }
